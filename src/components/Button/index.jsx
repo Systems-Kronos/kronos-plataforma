d468b8ebf84b52a7f2e34906d0ffbae72fff5ca6
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-09-08 10:35:24 -0300

    fix: adicionar disable quando onClick não for dado

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,13 @@
 import "./style.css";
 
-export default function Button({ text, onClick=undefined, variant }) {
-  return (
-        <button className={`btn ${variant} ${onClick ? "clicavel" : "nao-clicavel"}`} type="button" onClick={onClick}>
+export default function Button({ text, onClick = undefined, variant }) {
+    return (
+        <button
+            className={`btn ${variant} ${onClick ? "clicavel" : "nao-clicavel"}`}
+            type="button"
+            onClick={onClick}
+            disabled={!onClick}
+        >
             {text}
         </button>
     );
