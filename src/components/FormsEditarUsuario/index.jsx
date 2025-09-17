e84e8bb9d110300988ecc2d1cd7c87942c91ed2e
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-09-17 09:20:43 -0300

    feat: adicionando componente modal para editar membro

---
+++ src/components/FormsEditarUsuario/index.jsx
@@ -0,0 +1,52 @@
+import "./style.css";
+import CancelIcon from "@mui/icons-material/Cancel";
+import Button from "../Button";
+
+export default function FormsEditarMembro({ onClose }) {
+  return (
+    <div className="forms-modal">
+      <div className="modal-box">
+        <div className="modal-header">
+          <div>
+            <h2>Editar membro</h2>
+            <p>Preencha todas as informações para editar o cadastro!</p>
+          </div>
+          <CancelIcon style={{ color: "#370963", cursor: "pointer" }} onClick={onClose}/>
+        </div>
+
+        <div className="modal-body">
+          <form>
+            <label htmlFor="nomeCompleto">Nome Completo</label>
+            <input type="text" id="nomeCompleto" name="nomeCompleto" className="input-text" placeholder="Digite o nome completo"/>
+
+            <div className="form-row">
+              <div>
+                <label htmlFor="setor">Setor</label>
+                <select id="setor" name="setor" className="input-select">
+                  <option value="">Selecione</option>
+                </select>
+              </div>
+
+              <div>
+                <label htmlFor="gestao">Permitir gestão</label>
+                <select id="gestao" name="gestao" className="input-select">
+                  <option value="">Selecione</option>
+                </select>
+              </div>
+            </div>
+
+            <label htmlFor="email">E-mail</label>
+            <input type="text" id="email" name="email" className="input-text" placeholder="Digite o e-mail empresarial"></input>
+
+            <button className="button">Ativo</button>
+          </form>
+        </div>
+
+        <div className="modal-footer">
+          <Button texto="Cancelar" variant="secundario" onClick={onClose} />
+          <Button texto="Editar" variant="primario" onClick={() => console.log("Criar Tarefa")} />
+        </div>
+      </div>
+    </div>
+  );
+}
