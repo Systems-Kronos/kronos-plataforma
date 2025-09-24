3906b5d87e90e0f1750bf65fde19a07a275dd1ca
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-09-24 09:04:03 -0300

    feat: conectando o login a plataforma

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import { useEffect } from "react";
+import Layout from "./components/Layout";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Analises from "./pages/Analises/Analises";
@@ -8,25 +10,28 @@ import Reports from "./pages/Reports/Reports";
 import Tarefas from "./pages/Tarefas/Tarefas";
 
 export default function App() {
+  useEffect(() => {
+    localStorage.removeItem("token");
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+
         <Route
-          path="*"
           element={
-            <>
-              <Header />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/analises" element={<Analises />} />
-                <Route path="/membros-equipe" element={<Membros />} />
-                <Route path="/reports" element={<Reports />} />
-                <Route path="/tarefas" element={<Tarefas />} />
-              </Routes>
-            </>
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
           }
-        />
+        >
+          <Route path="/" element={<Home />} />
+          <Route path="/analises" element={<Analises />} />
+          <Route path="/membros-equipe" element={<Membros />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/tarefas" element={<Tarefas />} />
+        </Route>
       </Routes>
     </Router>
   );
