9cb93eef428ad1e3d1fb4ed8ca50e5a06eb62e69
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-09-30 12:17:05 -0300

    fix: salvando id do usuário ao realizar o login

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -2,8 +2,9 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
+  const usuarioId = localStorage.getItem("usuarioId");
 
-  if (!token) {
+  if (!token || !usuarioId) {
     return <Navigate to="/login" replace />;
   }
 
