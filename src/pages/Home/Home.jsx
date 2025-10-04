cf315325ce3bdbfa1764d339ad0546dd7c5b6495
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-10-03 21:50:23 -0300

    feat: conectando function para trazer os reports

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,27 +6,47 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import GroupsIcon from "@mui/icons-material/Groups";
 import AutoGraphIcon from "@mui/icons-material/AutoGraph";
 import { membrosPorGestor } from "../../service/membros";
+import { reportsPorGestor } from "../../service/reports";
 
 export default function Home() {
-    const [quantidade, setQuantidade] = useState(0);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const carregarMembros = async () => {
-        try {
-          const dados = await membrosPorGestor();
-          if (dados) {
-            setQuantidade(dados.quantidadeMembros);
-          }
-        } catch (err) {
-          console.error("Erro ao buscar membros", err);
-        } finally {
-          setLoading(false);
+  const [quantidade, setQuantidade] = useState(0);
+  const [reportsPendentes, setReportsPendentes] = useState(0);
+  const [loadingMembros, setLoadingMembros] = useState(true);
+  const [loadingReport, setLoadingReports] = useState(true);
+
+  useEffect(() => {
+    const carregarMembros = async () => {
+      try {
+        const dados = await membrosPorGestor();
+        if (dados) {
+          setQuantidade(dados.quantidadeMembros);
         }
-      };
-  
-      carregarMembros();
-    }, []);
+      } catch (err) {
+        console.error("Erro ao buscar membros", err);
+      } finally {
+        setLoadingMembros(false);
+      }
+    };
+
+    carregarMembros();
+  }, []);
+
+  useEffect(() => {
+    const carregarReports = async () => {
+      try {
+        const dados = await reportsPorGestor();
+        if (dados) {
+          setReportsPendentes(dados.pendentes);
+        }
+      } catch (err) {
+        console.error("Erro ao buscar reports", err);
+      } finally {
+        setLoadingReports(false);
+      }
+    };
+
+    carregarReports();
+  }, []);
 
   return (
     <div className={styles.boxContainer}>
@@ -35,13 +55,9 @@ export default function Home() {
       <div className={styles.cardsContainer}>
         <CardInformacoes
           titulo={"Membros"}
-          icone={
-            <GroupsIcon
-              style={{ color: "#E6B648", fontSize: 30, marginLeft: "-0.5vw" }}
-            />
-          }
+          icone={<GroupsIcon style={{ color: "#E6B648", fontSize: 30, marginLeft: "-0.5vw" }} />}
           descricao={"x membros nesse mês"}
-          numero={loading ? "--" : quantidade || "00"}
+          numero={loadingMembros ? "--" : quantidade || "00"}
         />
         <CardInformacoes
           titulo={"Tarefas Ativas"}
@@ -53,7 +69,7 @@ export default function Home() {
           titulo={"Reports Pendentes"}
           icone={<ErrorIcon style={{ color: "#E6B648" }} />}
           descricao={"x adicionado hoje"}
-          numero={"00"}
+          numero={loadingReport ? "--" : reportsPendentes || "00"}
         />
         <CardInformacoes
           titulo={"Produtividade"}
