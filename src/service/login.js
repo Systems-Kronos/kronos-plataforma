3051508df3c8b4e159fc92636dc35a1474089bc8
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-10-07 11:34:59 -0300

    fix: subindo alteração de itens guardados no localStorage

--- src/service/login.js
+++ src/service/login.js
@@ -9,7 +9,23 @@ export const login = async (cpf, senha) => {
       senha,
     });
 
-    return response.data;
+    const { token, usuarioId } = response.data;
+    localStorage.setItem("token", token);
+    localStorage.setItem("usuarioId", usuarioId);
+
+    const respGestor = await axios.get(`https://spring-api-sql.onrender.com/api/usuario/selecionarId/${usuarioId}`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    const empresaId = respGestor.data.empresa?.id;
+    if (empresaId) {
+      localStorage.setItem("empresaId", empresaId);
+    } else {
+      console.warn("Usuário não possui empresa vinculada");
+      localStorage.removeItem("empresaId");
+    }
+
+    return { ...response.data, empresaId };
   } catch (error) {
     console.error("Erro ao fazer login:", error);
     throw error;
