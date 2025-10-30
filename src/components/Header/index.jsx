39b1d3d1618f5c744eac25914a0c579fee497db2
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-10-30 19:20:30 -0300

    fix: fechar menu ao clicar no nome "Kronos"

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -82,7 +82,7 @@ export default function Header() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.titulo} onClick={() => navigate("/home")}>
+      <h1 className={styles.titulo} onClick={() => handleNavigation("/home")}>
         KRONOS
       </h1>
 
