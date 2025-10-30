f9d4433ed0f8947f062b5b1b820d06efc2de8104
Yasmin <yasmin.rodrigues@germinare.org.br>
2025-10-30 08:26:56 -0300

    fix: lint

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Noticias from "./pages/Noticias/Noticias";
 import Membros from "./pages/Membros/Membros";
-import Historico from "./pages/Historico/Historico";
 import Reports from "./pages/Reports/Reports";
 import Tarefas from "./pages/Tarefas/Tarefas";
 
@@ -25,7 +24,6 @@ export default function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/noticias" element={<Noticias />} />
           <Route path="/equipe" element={<Membros />} />
-          <Route path="/historico" element={<Historico />} />
           <Route path="/reports" element={<Reports />} />
           <Route path="/tarefas" element={<Tarefas />} />
         </Route>
